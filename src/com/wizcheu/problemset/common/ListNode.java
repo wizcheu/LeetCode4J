d7f608503da5013b8f29b656b2ea34ae6dfece2a
wizcheu <wizcheu@gmail.com>
2018-07-10 20:28:04 +0800

    add the common class ListNode

---
+++ src/com/wizcheu/problemset/common/ListNode.java
@@ -0,0 +1,22 @@
+package com.wizcheu.problemset.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        ListNode temp = this;
+        StringBuilder str = new StringBuilder();
+        while (temp != null) {
+            str.append(temp.val).append(" -> ");
+            temp = temp.next;
+        }
+        str.append(" -> NULL");
+        return str.toString();
+    }
+}
