8b0aa1effcb044d2f8a9158f7adc47035b37a38a
wizcheu <wizcheu@gmail.com>
2018-07-20 21:10:49 +0800

    solve the problem 72

---
+++ src/com/wizcheu/problemset/Solution072.java
@@ -0,0 +1,20 @@
+package com.wizcheu.problemset;
+
+/**
+ * @author wizcheu
+ * @date 2018/07/18 21:29
+ * @see <a href="https://leetcode.com/problems/edit-distance/description/">https://leetcode.com/problems/edit-distance/description/</a>
+ */
+public class Solution072 {
+
+    // Your solution here
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution072 st = new Solution072();
+        String word1 = "intention", word2 = "execution";
+        System.out.println(st.minDistance(word1, word2));
+    }
+}
